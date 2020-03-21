0076ae24ac2f887d714cbb57903f764aba3481d9
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-21 12:48:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(file_name)
   library = {:get_meaning => {}, :get_emoticon => {}}
   emoticons = YAML.load_file(file_name)
   emoticons.each do |moods, emotis|
-    
+    library[:get_meaning][emotis[1]] = moods
 end
 
 def get_japanese_emoticon
