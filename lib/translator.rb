ce323a6af9e5a54eab069913743370f58471c5e5
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-21 12:44:13 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "pry"
 def load_library(file_name)
   library = {:get_meaning => {}, :get_emoticon => {}}
   emoticons = YAML.load_file(file_name)
-  
+  pp emoticons
 end
 
 def get_japanese_emoticon
