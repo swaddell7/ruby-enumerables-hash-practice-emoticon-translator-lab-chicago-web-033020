cdc79bdd5017dd8a8cae8bb93560a9fb07c211c7
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-21 13:02:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_name, emoticon)
     end
   end
   if response == nil
-    "Sorry, that emoticon is not found."
+    return "Sorry, that emoticon is not found."
   end
   return response
 end
